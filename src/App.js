cbedcb54890a92b4acd6b82edf67910f17fc4712
Sumia Farooq <sumiafarooq@gmail.com>
2021-12-11 15:35:52 +0100

    new project file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import react from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const url = "https://api.imgflip.com/get_memes";
+  const [memesData, setMemesData] = useState(null);
+
+  const [meme, setmeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setMemesData(response.data.data.memes);
+    });
+  }, [url]);
+
+  function getMemeImage() {
+    const memeArray = memesData;
+
+    const randonNumber = Math.floor(Math.random() * memeArray.length);
+
+    const imageUrl = memeArray[randonNumber].url;
+    setmeme((preMeme) => ({
+      ...preMeme,
+      randomImage: imageUrl,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setmeme((preMeme) => ({
+      ...preMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Meme Fun Game</p>
       </header>
+      <div className="wrapper-content">
+        <div className="inputs">
+          <input
+            onChange={handleChange}
+            type="text"
+            name="topText"
+            value={meme.topText}
+            id=""
+            placeholder="Top text"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="bottomText"
+            value={meme.bottomText}
+            id=""
+            placeholder="Bottom text"
+          />
+          <button onClick={getMemeImage} className="get-image-btn">
+            Get a new image
+          </button>
+        </div>
+        <div className="meme">
+          <h2 className="meme-text top">{meme.topText}</h2>
+          <img className="meme-image" src={meme.randomImage} />
+          <h2 className="meme-text bottom">{meme.bottomText}</h2>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+//onClick={()=>getMemeImage} why does not work here?
